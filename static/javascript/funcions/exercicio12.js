0875f25747456d21fec80f6c52c708afb1f3b996
Igarrod <igarrod.genius@gmail.com>
2025-07-22 09:15:52 +0200

    first commit

--- static/javascript/funcions/exercicio12.js
+++
@@ -1,8 +0,0 @@
-/**
- *  #############################
- *  ## E J E R C I C I O   1 2 ##
- *  #############################
- *
- *  A partir del string 'España' obtener un array de personas no infectadas de ese país.
- *
- */
